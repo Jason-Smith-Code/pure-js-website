c127f437950ee3b6f8ed67d1795908dcfcc5a9a9
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-06-20 10:00:33 +0100

    added header, menu and logo

--- header.js
+++
@@ -1,3 +0,0 @@
-// Create the header
-// Create social media icons and links contact number and email
-// Create the logo
--- index.js
+++ index.js
@@ -1,12 +1,42 @@
-// Elements for index page go here
+// Header
+const header = document.createElement("header");
 
-const para = document.createElement("p");
-const node = document.createTextNode("This is new.");
-para.appendChild(node);
+// Header Logo
+const logoImage = document.createElement("img");
+const logoLocation = "./images/javascript-logo.png";
+logoImage.setAttribute("src", logoLocation);
+logoImage.setAttribute("alt", "logo");
+logoImage.setAttribute("height", 100);
+logoImage.setAttribute("width", 100);
 
-const element = document.getElementById("website");
-element.appendChild(para);
+// Header Menu
+const desktopNav = document.createElement("nav");
+const desktopUl = document.createElement("ul");
+desktopNav.appendChild(desktopUl);
+// Navgation items object constructor
+function MenuConstructor(name, location) {
+  this.name = name;
+  this.location = location;
+}
+// navigation items
+let navigationArray = [];
+const jsAbout = new MenuConstructor("about", "./about");
+const jsVariables = new MenuConstructor("variables", "./variables");
+const jsStatements = new MenuConstructor("statements", "./statements");
+const jsComments = new MenuConstructor("comments", "./comments");
+// add menu items to nav array
+navigationArray.push(jsAbout, jsVariables, jsStatements, jsComments);
+// map through the array and create anchors for each item
+navigationArray.map((item) => {
+  let desktopItem = document.createElement("li");
+  desktopUl.appendChild(desktopItem);
+  let a = document.createElement("a");
+  desktopItem.appendChild(a);
+  a.innerHTML = item.name;
+  a.setAttribute("href", item.location);
+});
 
-// Create the first section
-// Create a feature section
-// Create a feature section
+const website = document.getElementById("website");
+website.appendChild(header);
+header.appendChild(logoImage);
+header.appendChild(desktopNav);
